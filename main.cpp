b90d40512d9761edaee6953df81b7fa762442be6
tayndunl <tayndunl@ut.utm.edu>
2016-11-06 16:26:21 -0600

    added sum fucntion to main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,34 @@
+//Taylor Dunlap
+//main.cpp
+
 #include <iostream>
 
 using namespace std;
 
+int sum(int num);
+
 int main(){
 
+  int n;
+ 
   cout << "Hello World!" << endl;
 
+  cout << "Enter a number: ";
+  cin >> n;
+  sum(n);
+  cout << "Sum is: " << sum(n) << endl;
+  
   return 0;
 
+}
+
+int sum(int num){
+
+  int sum;
+  for(int i = 0; i < num; i++){
+
+    sum += i;
+  }
+
+  return sum;
 }
