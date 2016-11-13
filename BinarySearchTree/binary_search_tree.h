1f22d8fa8875b441485815de4464dd7dfd3c4bce
tayndunl <tayndunl@ut.utm.edu>
2016-11-13 01:51:00 -0600

    adding .h and main.cpp

---
+++ BinarySearchTree/binary_search_tree.h
@@ -0,0 +1,48 @@
+#ifndef BINARY_SEARCH_TREE
+#define BINARY_SEARCH_TREE
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Node{
+
+  // private:
+ public:
+  Node *left, *right, *current;
+  string key;
+  int data;
+
+  // public:
+  Node(int data);
+  Node(string key, int data);
+  ~Node();
+
+};
+
+
+class BST{
+
+ private:
+  Node *root;
+  void set_Helper(Node *current,string s, int value);
+  void print_Helper(Node *current);
+  void destructor_Helper(Node *current);
+  void remove_Helper(Node *current, int value);
+
+ public:
+  BST();
+  ~BST();
+  void set(string s, int value);
+  int find(string s);
+  void print();
+  void min();
+  void max();
+  void save_file(string fileName);
+  void remove(string fileName);
+
+};
+
+
+#endif
---
+++ BinarySearchTree/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "binary_search_tree.h"
+
+using namespace std;
+
+int main(){
+
+  BST tree;
+
+  tree.set("HELLO",4);
+
+
+  return 0;
+
+}
